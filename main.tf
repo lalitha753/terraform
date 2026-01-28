a83c861eebaf4d45bcec03f6774ac49f468d16c6
lalitha753 <lalithakurapati987@gmail.com>
2026-01-28 13:00:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "local_file" "pet" {
-  filename = var.pet_filename
+  filename = "${path.module}/${var.pet_filename}"
   content  = var.pet_content
 }
 
-resource "random_pet" "my_pet" {
+resource "random_pet" "mypet" {
   prefix   = var.random_pet_prefix
   separator = var.random_pet_separator
-  length    = var.random_pet_length
+  length   = var.random_pet_length
 }
+
