b9e4004c47d6492c67b3222caa83cc3767abb6f6
lalitha753 <lalithakurapati987@gmail.com>
2026-01-28 12:37:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
-terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
+resource "local_file" "pet" {
+  filename = var.pet_filename
+  content  = var.pet_content
 }
 
-resource "local_file" "demo" {
-  filename = "hello.txt"
-  content  = "This file was created by Terraform"
+resource "random_pet" "my_pet" {
+  prefix   = var.random_pet_prefix
+  separator = var.random_pet_separator
+  length    = var.random_pet_length
 }
